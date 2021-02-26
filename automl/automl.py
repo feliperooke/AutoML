8f04953b1cd6d0fde7fcf6e60c490e7d9622312d
Victor Aquiles <victoraqalencar@gmail.com>
2021-02-26 08:45:11 -0300

    TFT Initial setup

--- automl/automl.py
+++ automl/automl.py
@@ -1,14 +1,19 @@
+from lightgbm.sklearn import LGBMRegressor
 import pandas as pd
 import numpy as np
 import lightgbm as lgb
 import warnings
 from sklearn.metrics import mean_squared_error
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
+from pytorch_forecasting.metrics import QuantileLoss
+from pytorch_forecasting import TemporalFusionTransformer, TimeSeriesDataSet
 from .metrics import weighted_quantile_loss, weighted_absolute_percentage_error
 from .transformer import DataShift
 
 
 class AutoML:
-    def __init__(self, path, jobs=-1, fillnan='ffill', max_input_size=40, nlags=24):
+    def __init__(self, path, jobs=0, fillnan='ffill', max_input_size=40, nlags=24):
         """
         AutoML is an auto machine learning project with focus on predict
         time series using simple usage and high-level understanding over
@@ -35,6 +40,8 @@ class AutoML:
         self.nlags = nlags
 
         self.data = pd.read_csv(self.path)
+        self.target_label = None
+        self.index_label = None
         self.input_transformation = lambda x: x
         self.oldest_lag = 1
         self.quantiles = [.1, .5, .9]
@@ -44,6 +51,7 @@ class AutoML:
 
         self._data_shift = DataShift(nlags=self.nlags)
         self.X, self.y = self._transform_data('lightgbm')
+        self.training, self.validation = self._transform_data('TFT')
 
         # results obtained during evaluation
         self.evaluation_results = {}
@@ -66,14 +74,53 @@ class AutoML:
         """
         data = self.data.copy()
 
-        index_label, target_label = tuple(data.columns)
+        self.index_label, self.target_label = tuple(data.columns)
 
         # date column to datetime type
-        data[index_label] = pd.to_datetime(data[index_label])
+        data[self.index_label] = pd.to_datetime(data[self.index_label])
+        # removing timezone
+        data[self.index_label] = data[self.index_label].dt.tz_localize(None)
 
         # find the best past lags value
         self._data_shift.fit(data)
-        self.oldest_lag = max(self._data_shift.past_lags) + 1
+        self.oldest_lag = int(max(self._data_shift.past_lags)) + 1
+
+        if model == 'TFT':
+            # time index are epoch values
+            # data["time_idx"] = (data[self.index_label] - pd.Timestamp("1970-01-01")) // pd.Timedelta("1s")
+            data["time_idx"] = data.index
+            data['group_id'] = 'series'
+
+            max_prediction_length = self.oldest_lag
+            max_encoder_length = self.oldest_lag
+            training_cutoff = data["time_idx"].max() - max_prediction_length
+
+            training = TimeSeriesDataSet(
+                data[lambda x: x.time_idx <= training_cutoff],
+                time_idx="time_idx",
+                group_ids=["group_id"],
+                target=self.target_label,
+                min_encoder_length=0,
+                max_encoder_length=max_encoder_length,
+                min_prediction_length=1,
+                max_prediction_length=max_prediction_length,
+                static_categoricals=["group_id"],
+                time_varying_unknown_reals=[self.target_label],
+                # the docs says that the max_lag < max_encoder_length
+                lags={self.target_label: list(self._data_shift.past_lags[1:-1] + 1)},
+                add_relative_time_idx=True,
+                add_target_scales=True,
+                add_encoder_length=True,    
+                # allow_missings=True
+            )
+
+            # create validation set (predict=True) which means to predict the last max_prediction_length points in time
+            # for each series
+            validation = TimeSeriesDataSet.from_dataset(training, data, predict=True, stop_randomization=True)
+
+            processed_data = (training, validation)
+
+            return processed_data
 
         # add to the data the past lags
         data = self._data_shift.transform(data)
@@ -86,11 +133,13 @@ class AutoML:
         # adapt the data to the chosen model
         if model == 'lightgbm':
             x = data[x_labels]
-            y = data[target_label]
+            y = data[self.target_label]
 
-        return x, y
+            processed_data = (x, y)
 
-    def _evaluate_model(self, model, X_val, y_val, quantile=None):
+        return processed_data
+
+    def _evaluate_model(self, y_val, y_pred, quantile=None):
         """
         Evaluate a specifc model given the data to be tested.
 
@@ -100,8 +149,6 @@ class AutoML:
         :param quantile: Quantile value that will be evaluated.
 
         """
-
-        y_pred = model.predict(X_val)
 
         results = {}
 
@@ -120,6 +167,86 @@ class AutoML:
         """
         # Vamos fazer com os modelos sempre usando a api do Scikit Learn pq a gnt vai usar ele para o RandomSearch
 
+        # TFT
+
+        # configure network and trainer
+        # create dataloaders for model
+        batch_size = 128
+        train_dataloader = self.training.to_dataloader(train=True, batch_size=batch_size, num_workers=self.jobs)
+        val_dataloader = self.validation.to_dataloader(train=False, batch_size=batch_size * 10, num_workers=self.jobs)
+
+        pl.seed_everything(42)
+
+        tft = TemporalFusionTransformer.from_dataset(
+            self.training,
+            # not meaningful for finding the learning rate but otherwise very important
+            learning_rate=0.03,
+            hidden_size=16,  # most important hyperparameter apart from learning rate
+            # number of attention heads. Set to up to 4 for large datasets
+            attention_head_size=1,
+            dropout=0.1,  # between 0.1 and 0.3 are good values
+            hidden_continuous_size=8,  # set to <= hidden_size
+            output_size=len(self.quantiles),  # 3 quantiles by default
+            loss=QuantileLoss(self.quantiles),
+            # reduce learning rate if no improvement in validation loss after x epochs
+            reduce_on_plateau_patience=4,
+        )
+
+        early_stop_callback = EarlyStopping(monitor="val_loss", min_delta=1e-4, patience=10, verbose=False, mode="min")
+        lr_logger = LearningRateMonitor()
+
+        trainer = pl.Trainer(
+            max_epochs=25,
+            gpus=0,
+            weights_summary="top",
+            gradient_clip_val=0.1,
+            # limit_train_batches=30,  # coment in for training, running validation every 30 batches
+            # fast_dev_run=True,  # comment in to check that networkor dataset has no serious bugs
+            callbacks=[lr_logger, early_stop_callback],
+        )
+
+        tft = TemporalFusionTransformer.from_dataset(
+            self.training,
+            learning_rate=0.3,
+            hidden_size=16,
+            attention_head_size=1,
+            dropout=0.1,
+            hidden_continuous_size=8,
+            output_size=len(self.quantiles),  # 3 quantiles by default
+            loss=QuantileLoss(self.quantiles),
+            reduce_on_plateau_patience=4,
+        )
+
+        # fit network
+        trainer.fit(
+            tft,
+            train_dataloader=train_dataloader,
+            val_dataloaders=val_dataloader,
+        )
+
+        # evaluate the TFT
+        
+        self.evaluation_results['TFT'] = {}
+
+        # evaluate the models on the last max lag period
+        y_val = self.y[-self.oldest_lag:]
+        # default values
+        y_pred = tft.predict(self.validation, mode='prediction').numpy()[0]
+        self.evaluation_results['TFT']['default'] = self._evaluate_model(y_val, y_pred)
+
+        # quantile values
+        y_pred = tft.predict(self.validation, mode='quantiles').numpy()[0]
+
+        for i in range(len(self.quantiles)):
+            quantile = self.quantiles[i]
+            q_pred = y_pred[:, i]
+            self.evaluation_results['TFT'][str(quantile)] = self._evaluate_model(y_val, q_pred, quantile)
+        
+
+        # LightGBM
+
+        self.evaluation_results['LightGBM'] = {}
+
         # using quantile prediction as default
         quantile_params = {
             'objective': 'quantile',
@@ -128,10 +255,19 @@ class AutoML:
         quantile_models = [lgb.LGBMRegressor(alpha=quantil, **quantile_params)
                            for quantil in self.quantiles]
 
-        best_model = lgb.LGBMRegressor()  # Temp
+        lgbm_model = lgb.LGBMRegressor()  # Temp
 
-        self.model = best_model
+        self.model = lgbm_model
         self.quantile_models = quantile_models
+        self._trainer()
+
+        # Choose the best model comparing the default prediction metric results
+        min_metric = min([(x[0], x[1]['default']['wape']) for x in self.evaluation_results.items()])
+
+        if min_metric[0] == 'LightGBM':
+            self.model = lgbm_model
+        elif min_metric[0] == 'TFT':
+            self.model = tft
 
     def _trainer(self):
         """
@@ -139,25 +275,26 @@ class AutoML:
 
         """
 
-        # train data shifted by the max lag period
-        X_train, y_train = self.X[:-self.oldest_lag], self.y[:-self.oldest_lag]
+        if isinstance(self.model, LGBMRegressor):
+            # train data shifted by the max lag period
+            X_train, y_train = self.X[:-self.oldest_lag], self.y[:-self.oldest_lag]
 
-        self.model.fit(X_train, y_train)
+            self.model.fit(X_train, y_train)
 
-        for quantile_model in self.quantile_models:
-            quantile_model.fit(X_train, y_train)
+            for quantile_model in self.quantile_models:
+                quantile_model.fit(X_train, y_train)
 
-        # evaluate the models on the last max lag period
-        X_val, y_val = self.X[-self.oldest_lag:], self.y[-self.oldest_lag:]
+            # evaluate the models on the last max lag period
+            X_val, y_val = self.X[-self.oldest_lag:], self.y[-self.oldest_lag:]
 
-        # default model
-        self.evaluation_results['default'] = self._evaluate_model(
-            self.model, X_val, y_val)
+            # default model
+            y_pred = self.model.predict(X_val)
+            self.evaluation_results['LightGBM']['default'] = self._evaluate_model(y_val, y_pred)
 
-        # quantile models
-        for quantile, model in zip(self.quantiles, self.quantile_models):
-            self.evaluation_results[str(quantile)] = self._evaluate_model(
-                model, X_val, y_val, quantile)
+            # quantile models
+            for quantile, model in zip(self.quantiles, self.quantile_models):
+                y_pred = model.predict(X_val)
+                self.evaluation_results['LightGBM'][str(quantile)] = self._evaluate_model(y_val, y_pred, quantile)
 
     def predict(self, X, future_steps, quantile=False):
         """
@@ -177,28 +314,72 @@ class AutoML:
             raise Exception(f'''Error, to make a prediction X needs to be at
                                 least {self.oldest_lag} items long''')
 
-        cur_X = self._data_shift.transform(X.copy())
-        cur_X = cur_X[self._data_shift.past_labels].values
+        # Pre-process data
+        if isinstance(self.model, TemporalFusionTransformer):
+            time_idx = list(range(len(X))) # refact to use real time idx
+            X[self.index_label] = pd.to_datetime(X[self.index_label])
+            X[self.index_label] = X[self.index_label].dt.tz_localize(None)
+            X["time_idx"] = time_idx
+            X['group_id'] = 'series'
+
+            temp_data = self.data.iloc[-(self.oldest_lag+1):].copy()
+            
+            cur_X = temp_data.append(X, ignore_index=True)
+            time_idx = list(range(len(cur_X))) # refact to use real time idx
+            cur_X["time_idx"] = time_idx
+
+            cur_X.index = list(range(len(cur_X)))
+            
+        else:
+            cur_X = self._data_shift.transform(X.copy())
+            cur_X = cur_X[self._data_shift.past_labels].values
         y = []
 
-        for i in range(future_steps):
+        # Prediction
+        if isinstance(self.model, TemporalFusionTransformer):
+            mode = 'quantiles' if quantile else 'prediction'
 
-            if quantile:  # predict with quantile models
-                predict = []
-                for quantile_model in self.quantile_models:
-                    predict.append(quantile_model.predict(
-                        cur_X[-1].reshape(1, -1)))
-                y.append(predict)
+            date_step = cur_X[self.index_label].iloc[-1] - cur_X[self.index_label].iloc[-2]
+            y = []
+            for _ in range(future_steps):
+                predict = self.model.predict(cur_X, mode=mode)[0][0]
+                if quantile:
+                    y.append(predict.numpy().tolist())
+                    new_value = y[-1][1] # get quantil 0.5
+                else:
+                    y.append(float(predict.numpy()))
+                    new_value = y[-1]
 
-                # choose the median prediction to feed the new predictions
-                predict = predict[1]
+                new_date = cur_X[self.index_label].iloc[-1] + date_step                
 
-            else:  # predict with mean model
-                predict = self.model.predict(cur_X[-1].reshape(1, -1))
-                y.append(predict)
-            # y.append(self.model.predict(np.squeeze(self.input_transformation(cur_X[-self.oldest_lag:]))))
-            new_input = np.append(cur_X[-1][1:], predict, axis=0)
-            cur_X = np.append(cur_X, [new_input], axis=0)
+                # auto feed the model to perform the next prediction
+                new_entry = {
+                    self.index_label: new_date,
+                    self.target_label: new_value,
+                    'time_idx': cur_X['time_idx'].iloc[-1] + 1,
+                    'group_id': 'series'
+                    }
+                cur_X = cur_X.append(new_entry, ignore_index=True)
+
+        else:
+            for _ in range(future_steps):
+
+                if quantile:  # predict with quantile models
+                    predict = []
+                    for quantile_model in self.quantile_models:
+                        predict.append(quantile_model.predict(
+                            cur_X[-1].reshape(1, -1)))
+                    y.append(predict)
+
+                    # choose the median prediction to feed the new predictions
+                    predict = predict[1]
+
+                else:  # predict with mean model
+                    predict = self.model.predict(cur_X[-1].reshape(1, -1))
+                    y.append(predict)
+                # y.append(self.model.predict(np.squeeze(self.input_transformation(cur_X[-self.oldest_lag:]))))
+                new_input = np.append(cur_X[-1][1:], predict, axis=0)
+                cur_X = np.append(cur_X, [new_input], axis=0)
 
         return y
 
@@ -213,7 +394,10 @@ class AutoML:
             Use quantile models instead of the mean based.
 
         """
-        return self.predict(self.data, future_steps, quantile=quantile)
+        if isinstance(self.model, TemporalFusionTransformer):
+            return self.predict(self.data, future_steps, quantile=quantile)
+        else:
+            return self.predict(self.data, future_steps, quantile=quantile)
 
     def add_new_data(self, new_data_path, append=True):
         """
