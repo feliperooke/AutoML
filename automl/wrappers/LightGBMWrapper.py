e3544558901a0c0611097e6e4acffda9fda6515b
LucasRibP <44446242+LucasRibP@users.noreply.github.com>
2021-03-26 14:22:30 -0300

    Fix base LightGBMWrapper

--- automl/wrappers/LightGBMWrapper.py
+++ automl/wrappers/LightGBMWrapper.py
@@ -2,8 +2,8 @@ from .BaseWrapper import BaseWrapper
 
 
 class LightGBMWrapper(BaseWrapper):
-    __init__(self, quantiles)
-    super.__init__(quantiles)
+    def __init__(self, quantiles):
+        super.__init__(quantiles)
 
     def transform_data(self, data, past_lags, index_label, target_label):
         pass
