58f0a8683eb0ef30b72dace803897190783518e6
LucasRibP <44446242+LucasRibP@users.noreply.github.com>
2021-06-08 16:07:54 -0300

    Added quantile implementation of XGBoost

--- automl/wrappers/XGBoostWrapper.py
+++ automl/wrappers/XGBoostWrapper.py
@@ -1,9 +1,86 @@
 from .BaseWrapper import BaseWrapper
 from tqdm import tqdm
+from functools import partial
 import copy
 import numpy as np
 import xgboost as xgb
+from xgboost.sklearn import XGBRegressor
 from sklearn.model_selection import train_test_split
+
+# Code by: https://towardsdatascience.com/regression-prediction-intervals-with-xgboost-428e0a018b
+
+
+class XGBQuantile(XGBRegressor):
+    def __init__(self, quant_alpha=0.95, quant_delta=1.0, quant_thres=1.0, quant_var=1.0, base_score=0.5, booster='gbtree', colsample_bylevel=1,
+                 colsample_bytree=1, gamma=0, learning_rate=0.1, max_delta_step=0, max_depth=3, min_child_weight=1, missing=None, n_estimators=100,
+                 n_jobs=1, nthread=None, objective='reg:linear', random_state=0, reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=None, silent=True, subsample=1):
+        self.quant_alpha = quant_alpha
+        self.quant_delta = quant_delta
+        self.quant_thres = quant_thres
+        self.quant_var = quant_var
+
+        super().__init__(base_score=base_score, booster=booster, colsample_bylevel=colsample_bylevel,
+                         colsample_bytree=colsample_bytree, gamma=gamma, learning_rate=learning_rate, max_delta_step=max_delta_step,
+                         max_depth=max_depth, min_child_weight=min_child_weight, missing=missing, n_estimators=n_estimators,
+                         n_jobs=n_jobs, nthread=nthread, objective=objective, random_state=random_state,
+                         reg_alpha=reg_alpha, reg_lambda=reg_lambda, scale_pos_weight=scale_pos_weight, seed=seed,
+                         silent=silent, subsample=subsample)
+
+        self.test = None
+
+    def fit(self, X, y):
+        super().set_params(objective=partial(XGBQuantile.quantile_loss, alpha=self.quant_alpha,
+                                             delta=self.quant_delta, threshold=self.quant_thres, var=self.quant_var))
+        super().fit(X, y)
+        return self
+
+    def predict(self, X):
+        return super().predict(X)
+
+    def score(self, X, y):
+        y_pred = super().predict(X)
+        score = XGBQuantile.quantile_score(y, y_pred, self.quant_alpha)
+        score = 1./score
+        return score
+
+    @staticmethod
+    def quantile_loss(y_true, y_pred, alpha, delta, threshold, var):
+        x = y_true - y_pred
+        grad = (x < (alpha-1.0)*delta)*(1.0-alpha) - ((x >= (alpha-1.0)
+                                                       * delta) & (x < alpha*delta))*x/delta-alpha*(x > alpha*delta)
+        hess = ((x >= (alpha-1.0)*delta) & (x < alpha*delta))/delta
+
+        grad = (np.abs(x) < threshold)*grad - (np.abs(x) >= threshold) * \
+            (2*np.random.randint(2, size=len(y_true)) - 1.0)*var
+        hess = (np.abs(x) < threshold)*hess + (np.abs(x) >= threshold)
+        return grad, hess
+
+    @staticmethod
+    def original_quantile_loss(y_true, y_pred, alpha, delta):
+        x = y_true - y_pred
+        grad = (x < (alpha-1.0)*delta)*(1.0-alpha)-((x >= (alpha-1.0)
+                                                     * delta) & (x < alpha*delta))*x/delta-alpha*(x > alpha*delta)
+        hess = ((x >= (alpha-1.0)*delta) & (x < alpha*delta))/delta
+        return grad, hess
+
+    @staticmethod
+    def quantile_score(y_true, y_pred, alpha):
+        score = XGBQuantile.quantile_cost(x=y_true-y_pred, alpha=alpha)
+        score = np.sum(score)
+        return score
+
+    @staticmethod
+    def quantile_cost(x, alpha):
+        return (alpha-1.0)*x*(x < 0)+alpha*x*(x >= 0)
+
+    @staticmethod
+    def get_split_gain(gradient, hessian, l=1):
+        split_gain = list()
+        for i in range(gradient.shape[0]):
+            split_gain.append(np.sum(gradient[:i])/(np.sum(hessian[:i])+l)+np.sum(
+                gradient[i:])/(np.sum(hessian[i:])+l)-np.sum(gradient)/(np.sum(hessian)+l))
+
+        return np.array(split_gain)
 
 
 class XGBoostWrapper(BaseWrapper):
@@ -29,9 +106,9 @@ class XGBoostWrapper(BaseWrapper):
         self.training = (X_train, y_train)
         self.validation = (X_test, y_test)
 
-    def train(self, model_params, quantile_params):
+    def train(self, model_params):
 
-        self.qmodels = [xgb.XGBRegressor(alpha=quantil, **model_params, **quantile_params)
+        self.qmodels = [XGBQuantile(quant_alpha=quantil, **model_params)
                         for quantil in self.quantiles]
 
         for qmodel in self.qmodels:
@@ -122,11 +199,6 @@ class XGBoostWrapper(BaseWrapper):
         'n_estimators': 80,
     }, ]
 
-    quantile_params = {
-        'objective': 'quantile',
-        'metric': 'quantile',
-    }
-
     @staticmethod
     def _evaluate(auto_ml, cur_wrapper):
         prefix = 'XGBoost'
@@ -139,7 +211,7 @@ class XGBoostWrapper(BaseWrapper):
 
         for c, params in tqdm(enumerate(XGBoostWrapper.params_list)):
             auto_ml.evaluation_results[prefix+str(c)] = {}
-            cur_wrapper.train(params, XGBoostWrapper.quantile_params)
+            cur_wrapper.train(params)
 
             y_pred = np.array(cur_wrapper.predict(
                 cur_wrapper.validation[0], max(auto_ml.important_future_timesteps)))[:, [-(n-1) for n in auto_ml.important_future_timesteps]]
